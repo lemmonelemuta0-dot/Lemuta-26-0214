ddc298a5b561ddbfae1608235d6d032f24e17f71
lemmonelemuta0-dot <lemmonelemuta0@gmail.com>
2026-06-23 06:48:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-setTimeout(function () {
+window.onload = function(){
+    alert("Welcome to Smart Greenhouse Management System 🌱");
+};
+
+setTimeout(function(){
     document.getElementById("promoBox").style.display = "block";
 }, 3000);
 
-function closePromo() {
+function closePromo(){
     document.getElementById("promoBox").style.display = "none";
 }
