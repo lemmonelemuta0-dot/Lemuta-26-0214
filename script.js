bae93ed84fdee8d7b7ce0f849c64e6b3c7dd3761
lemmonelemuta0-dot <lemmonelemuta0@gmail.com>
2026-06-23 09:39:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-document.getElementById("userForm").addEventListener("submit", function(event){
-    event.preventDefault();
+// WELCOME POPUP
+window.onload = function(){
+    document.getElementById("welcomePopup").style.display = "flex";
 
-    // show popup instead of alert
-    document.getElementById("formPopup").style.display = "block";
+    setTimeout(function(){
+        document.getElementById("promoBox").style.display = "flex";
+    }, 3000);
+};
 
-    // optional: clear form after submit
+// FORM POPUP
+document.getElementById("userForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("formPopup").style.display = "flex";
     this.reset();
 });
+
+// CLOSE FUNCTIONS
+function closePromo(){
+    document.getElementById("promoBox").style.display = "none";
+}
 
 function closeFormPopup(){
     document.getElementById("formPopup").style.display = "none";
