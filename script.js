7b0a9934cfcb541962aa1027f81da037e09615af
lemmonelemuta0-dot <lemmonelemuta0@gmail.com>
2026-06-23 09:05:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.onload = function(){
-    alert("Welcome to Smart Greenhouse Management System 🌱");
-};
-
-setTimeout(function(){
-    document.getElementById("promoBox").style.display = "block";
-}, 3000);
-
-function closePromo(){
-    document.getElementById("promoBox").style.display = "none";
-}
 document.getElementById("userForm").addEventListener("submit", function(event){
     event.preventDefault();
-    alert("Thank you! Your details have been submitted successfully.");
+
+    // show popup instead of alert
+    document.getElementById("formPopup").style.display = "block";
+
+    // optional: clear form after submit
+    this.reset();
 });
+
+function closeFormPopup(){
+    document.getElementById("formPopup").style.display = "none";
+}
