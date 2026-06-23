06604873a712943623e84b90661541fc2d19b9b7
lemmonelemuta0-dot <lemmonelemuta0@gmail.com>
2026-06-23 06:38:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+setTimeout(function () {
+    document.getElementById("promoBox").style.display = "block";
+}, 3000);
 
+function closePromo() {
+    document.getElementById("promoBox").style.display = "none";
+}
